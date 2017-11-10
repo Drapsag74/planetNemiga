80962ea2438e95f1de447e0d9ec7f056bda89e4a
Unknown <gaspard7426@gmail.com>
2017-11-10 20:23:58 +0100

    ajout Semantic
    ajout de semantic ui pour le css, changement home, mise en place controller

---
+++ config/config.js
@@ -0,0 +1,7 @@
+config = {};
+
+config.web = {};
+
+config.web.port = process.env.PORT || '3000';
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.redirect("/home");
 });
 
-module.exports = router;
+router.get('/home', (req, res, next) => {
+  res.render('home', { title: 'Planet Nemiga' });
+});
+
+
+
+
+
+
+
+
+
+
+module.exports =  router;
