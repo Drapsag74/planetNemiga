d576fb2da310c32e1eb37b3917e3836ad49c7661
Unknown <sacha.belhassen@gmail.com>
2017-12-09 17:52:29 +0100

    create.sql

--- database/createdb.sql
+++ database/createdb.sql
@@ -1,48 +1,83 @@
+DROP DATABASE IF EXISTS planetNemiga;
+CREATE DATABASE planetNemiga;
 DROP DATABASE IF EXISTS "planetNemiga";
 CREATE DATABASE "planetNemiga";
 
+\c planetNemiga;
 \c "planetNemiga";
 
-CREATE SCHEMA users;
-CREATE SCHEMA challenges;
 
-CREATE TABLE challenges.chapitres (
-	titre varchar PRIMARY KEY,
-	matiere varchar
+CREATE TABLE chapitre(
+  titre VARCHAR PRIMARY KEY,
+  matiere VARCHAR(5)
 );
 
-CREATE TABLE challenges.enigmes (
+CREATE TABLE enigme(
+  id SERIAL PRIMARY KEY,
+  titre VARCHAR(70),
+  enonce text,
+	nbReponse INTEGER,
+	niveauDiff INTEGER
+);
+
+CREATE TABLE reponse(
+  id SERIAL PRIMARY KEY,
+  texte text,
+  idEnigme INTEGER REFERENCES enigme(id)
+);
+
+CREATE TABLE personnage(
+  categorie VARCHAR PRIMARY KEY,
+  sexe VARCHAR,
+	description text,
+  image VARCHAR
+);
+
+CREATE TABLE ecole(
+	nomEcole VARCHAR,
+	codePostal INTEGER,
+	ville VARCHAR,
+	PRIMARY KEY(nomEcole, codePostal)
+);
+
+CREATE TABLE classe(
 	id SERIAL PRIMARY KEY,
-	titre varchar,
-	enonce text
-
+	nom VARCHAR(20)
 );
 
-CREATE TABLE challenges.reponses (
-	id SERIAL PRIMARY KEY,
-	texte text,
-	idEnigme integer REFERENCES challenges.enigmes(id)
+CREATE TABLE joueur(
+  id SERIAL PRIMARY KEY,
+	pseudo VARCHAR(15) UNIQUE,
+	mail VARCHAR(40),
+	motDePasse VARCHAR(50),
+	personnage VARCHAR REFERENCES personnage(categorie),
+	ecole VARCHAR REFERENCES ecole(nomEcole),
+	classe INTEGER REFERENCES classe(id)
+);
+CREATE TABLE informatique(
+	niveau INTEGER PRIMARY KEY
+);
+CREATE TABLE progression(
+	informatique INTEGER REFERENCES informatique(niveau),
+	chapitre VARCHAR REFERENCES chapitre(titre),
+	joueur INTEGER REFERENCES joueur(id),
+	xp INTEGER,
+	timestand INTEGER,
+	PRIMARY KEY(informatique, chapitre, joueur)
 );
 
-CREATE TABLE users.personnages (
+INSERT INTO personnage VALUES('Gaspard the god', 'homme','/images/logo.png');
+INSERT INTO personnage VALUES('Eddy la tchoin', 'indéfinie','/images/logo.png');
+INSERT INTO personnage VALUES('Clery','homme','/images/logo.png');
+INSERT INTO personnage VALUES('Zak la menace', 'homme','/images/logo.png');
+INSERT INTO personnage VALUES('mathcatch', 'indéfinie','/images/logo.png');
+INSERT INTO personnage VALUES('sachatouille','homme','/images/logo.png');
 
-	classe VARCHAR PRIMARY KEY,
-	sexe VARCHAR,
-	image VARCHAR
-);
 
-
-CREATE TABLE users.joueurs (
-	id BIGSERIAL PRIMARY KEY,
-	pseudo VARCHAR UNIQUE,
-	mail VARCHAR,
-	motDePasse VARCHAR
-);
-
-INSERT INTO challenges.enigmes VALUES(DEFAULT, 'enigme du rois gaspard', 'la meilleur enigme du monde entier');
-INSERT INTO users.personnages VALUES('Gaspard the god', 'homme','/images/logo.png');
-INSERT INTO users.personnages VALUES('Eddy la tchoin', 'indéfinie','/images/logo.png');
-INSERT INTO users.personnages VALUES('Clery','homme','/images/logo.png');
-INSERT INTO users.personnages VALUES('Zak la menace', 'homme','/images/logo.png');
-INSERT INTO users.personnages VALUES('mathcatch', 'indéfinie','/images/logo.png');
-INSERT INTO users.personnages VALUES('sachatouille','homme/femme','/images/logo.png');
+INSERT INTO enigme VALUES(DEFAULT, 'enigme du rois gaspard', 'la meilleur enigme du monde entier');
+INSERT INTO personnage VALUES('Gaspard the god', 'homme','/images/logo.png');
+INSERT INTO personnage VALUES('Eddy la tchoin', 'indéfinie','/images/logo.png');
+INSERT INTO personnage VALUES('Clery','homme','/images/logo.png');
+INSERT INTO personnage VALUES('Zak la menace', 'homme','/images/logo.png');
+INSERT INTO personnage VALUES('mathcatch', 'indéfinie','/images/logo.png');
+INSERT INTO personnage VALUES('sachatouille','homme','/images/logo.png');
