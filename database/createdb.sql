8727079611141a26e36ca3d78c944fd9826a2127
Unknown <sacha.belhassen@gmail.com>
2018-01-04 11:36:42 +0100

    insertion ecoles

--- database/createdb.sql
+++ database/createdb.sql
@@ -33,52 +33,7 @@ CREATE TABLE users.personnages(
   image VARCHAR
 );
 
-/*
-  CREATION de ecoles a partir d'une bdd existante
-*/
 
-
-CREATE TABLE users.ecoles_tmp(
-  numero_uai VARCHAR,
-	appellation_officielle VARCHAR,
-  denomination_principale VARCHAR,
-  patronyme_uai VARCHAR,
-  secteur_public_prive_libe VARCHAR,
-  adresse_uai VARCHAR,
-  lieu_dit_uai VARCHAR,
-  boite_postale_uai INTEGER,
-	code_postal_uai INTEGER,
-	localite_acheminement_uai VARCHAR,
-  coordonnee_x INTEGER,
-  coordonnee_y INTEGER,
-  appariement INTEGER,
-  localisation VARCHAR,
-  nature_uai VARCHAR,
-  nature_uai_libe VARCHAR,
-  etat_etablissement VARCHAR,
-  type VARCHAR,
-	PRIMARY KEY(appellation_officielle, code_postal_uai)
-);
-
-\copy  users.ecoles_tmp (numero_uai, appellation_officielle, denomination_principale, patronyme_uai, secteur_public_prive_libe, adresse_uai, lieu_dit_uai, boite_postale_uai, code_postal_uai, localite_acheminement_uai, coordonnee_x, coordonnee_y, appariement, localisation, nature_uai, nature_uai_libe,etat_etablissement, type)
-FROM 'DEPP-etab-1D2D.csv' DELIMITER ';' HEADER CSV ;
-
-ALTER TABLE users.ecoles_tmp DROP COLUMN numero_uai;
-ALTER TABLE users.ecoles_tmp DROP COLUMN patronyme_uai;
-ALTER TABLE users.ecoles_tmp DROP COLUMN secteur_public_prive_libe;
-ALTER TABLE users.ecoles_tmp DROP COLUMN adresse_uai;
-ALTER TABLE users.ecoles_tmp DROP COLUMN lieu_dit_uai;
-ALTER TABLE users.ecoles_tmp DROP COLUMN boite_postale_uai;
-ALTER TABLE users.ecoles_tmp DROP COLUMN coordonnee_x;
-ALTER TABLE users.ecoles_tmp DROP COLUMN coordonnee_y;
-ALTER TABLE users.ecoles_tmp DROP COLUMN appariement;
-ALTER TABLE users.ecoles_tmp DROP COLUMN localisation;
-ALTER TABLE users.ecoles_tmp DROP COLUMN nature_uai;
-ALTER TABLE users.ecoles_tmp DROP COLUMN nature_uai_libe;
-ALTER TABLE users.ecoles_tmp DROP COLUMN etat_etablissement;
-ALTER TABLE users.ecoles_tmp DROP COLUMN type;
-
-DELETE FROM users.ecoles_tmp WHERE denomination_principale != 'COLLEGE' OR denomination_principale != 'COLLEGE PRIVE';
 
 CREATE TABLE users.ecoles(
   nomEcole VARCHAR,
@@ -87,10 +42,12 @@ CREATE TABLE users.ecoles(
   PRIMARY KEY (nomEcole, codePostal)
 );
 
-INSERT INTO users.ecoles(nomEcole, codePostal, ville)
-SELECT appellation_officielle, code_postal_uai, localite_acheminement_uai FROM users.ecoles_tmp;
+INSERT INTO users.ecoles(nomEcole, codePostal, ville) VALUES ('Champollion',38000, 'Grenoble');
+INSERT INTO users.ecoles(nomEcole, codePostal, ville) VALUES ('Stendhal',38000, 'Grenoble');
+INSERT INTO users.ecoles(nomEcole, codePostal, ville) VALUES ('Bayard',38000, 'Grenoble');
+INSERT INTO users.ecoles(nomEcole, codePostal, ville) VALUES ('Fantin Latour',38000, 'Grenoble');
+INSERT INTO users.ecoles(nomEcole, codePostal, ville) VALUES ('Jules Flandrin',38700, 'Corenc');
 
-DROP TABLE users.ecoles_tmp;
 
 CREATE TABLE users.classes(
 	id SERIAL PRIMARY KEY,
@@ -108,9 +65,11 @@ CREATE TABLE users.joueurs(
 	classe INTEGER REFERENCES users.classes(id),
 	FOREIGN KEY(nomEcole, codePostal) REFERENCES users.ecoles(nomEcole, codePostal)
 );
+
 CREATE TABLE challenges.informatiques(
 	niveau INTEGER PRIMARY KEY
 );
+
 CREATE TABLE users.progressions(
 	informatique INTEGER REFERENCES challenges.informatiques(niveau),
 	chapitre VARCHAR REFERENCES challenges.chapitres(titre),
